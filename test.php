aeba02b23e78168f190666a55e88d63df3ec52d5
Kacper <kacpersloka@o2.pl>
2022-10-19 09:37:40 +0200

    work

--- test.php
+++ test.php
@@ -11,6 +11,9 @@
   <link rel="stylesheet" href="css/home-main.css">
   <script type="module" src="js/home-main.js"></script>
 
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
